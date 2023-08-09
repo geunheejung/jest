3761edcc7afca43e8fab545e2d6e0efeabbd5e58
geunheejung <geunhee0212@gmail.com>
2023-08-09 15:28:23 +0900

    Reducer Composition with Arrays.

--- store.test.js
+++ store.test.js
@@ -125,25 +125,67 @@ const testToggleTodo = () => {
 testToggleTodo();
 
 describe("test todo reducer.", () => {
-  const todos = (stateBefore, action) => {
+  // 동일한 상수를 갖는 것 외에는 switch 논리에서 중복은 없다.
+  // todo reducer는 개별 할 일에 대한 변견 사항을 처리하고
+  // todos reducer는 배열의 변경 사항을 처리한다.
+  // switch 문을 제외하고는 이들 사이에 공유되는 코드는 없다.
+  // 로직이 더 복잡해지면(처리하는 작업이 더 많아지면) todo reducer는 커지지만 todos reducder는 거의 동일하게 유지된다.
+  // 이것이 분리하는 이유이다. -> 배열의 대해 요소를 추가,삭제,변경에 대해서는 비슷하니.
+  // 분리하지 않으면 개별 todo를 변경하는 작업을 더 추가 하려면 인덱스에서 할 일을 변경하는 코드를 복사 후 붙여넣어야 하는데,
+  // 이는 중복이 된다. ->
+  const todo = (state, action) => {
+    switch (action.type) {
+      case "ADD_TODO":
+        return {
+          id: action.id,
+          text: action.text,
+          completed: false,
+        };
+      case "TOGGLE_TODO":
+        if (state.id !== action.id) return state;
+
+        console.log("state -->", state);
+        return {
+          ...state,
+          completed: !state.completed,
+        };
+      default:
+        return state;
+    }
+  };
+
+  // 개별 할 일에 대해서 따로 reducer로 분리하지 않고 함수로 분리할 수 있다.
+  // ex) addTodo(state, action) 등.
+  /*
+  여기에는 엄격한 규칙은 없다. 합리적이라면 무엇이든 해도 된다.
+  하지만 스위치 문이 중복이라는것은 잘못됐다.
+  대규모 앱에서는 모든 Reducer 안에 Switch 있고, 그 중 일부는 동일한 작업을 처리할 것이다.
+  이는 정상적인 현상이며, 추상화할 필요가 없다.
+  위 처럼 함수로 분리할 경우 시각적 복제(스위치 문)를 논리적 복제(배열의 단일 항목을 업데이트하는 코드) 와 교환하는 것이다.
+  
+  */
+
+  const todos = (state = [], action) => {
     switch (action.type) {
       case "ADD_TODO":
         return [
-          ...stateBefore,
-          {
-            id: action.id,
-            text: action.text,
-            completed: false,
-          },
+          ...state,
+          todo(undefined, action), // addTodo여서 그런가 보통은 기존의 state를 토대로 무언가를 처리하는데, 단일한 아이템에서 add의 경우 생성만 해주면 되니깐?
+          // {
+          //   id: action.id,
+          //   text: action.text,
+          //   completed: false,
+          // },
         ];
       case "TOGGLE_TODO":
-        return stateBefore.map((row) => {
-          if (row.id === action.id) return { ...row, completed: true };
+        return state.map((row) => todo(row, action)); // 개별 아이템에 대한 처리를 옮김.
+      // return stateBefore.map((row) => {
+      //   if (row.id === action.id) return { ...row, completed: true };
 
-          return row;
-        });
+      //   return row;
+      // });
       default:
-        return stateBefore;
+        return state;
     }
   };
 
@@ -205,6 +247,6 @@ describe("test todo reducer.", () => {
     deepFreeze(stateBefore);
     deepFreeze(toggleTodoAction);
 
-    expect(todos(stateAfter, toggleTodoAction)).toEqual(stateAfter);
+    expect(todos(stateBefore, toggleTodoAction)).toEqual(stateAfter);
   });
 });
